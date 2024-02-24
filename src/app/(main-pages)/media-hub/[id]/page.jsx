6fd4828ac9916ad861babca5e870ed211158c794
Dickson Ma <dickson_ma@jstar.email>
2024-02-25 02:30:31 +0800

    update

--- src/app/(main-pages)/media-hub/[id]/page.jsx
+++ src/app/(main-pages)/media-hub/[id]/page.jsx
@@ -2,24 +2,21 @@ import { promises as fs } from "fs";
 import ShareBtn from "./sharebtn";
 import mediaNews from "../../../../data/mediahub.json";
 
-// export async function generateMetadata({ params }) {
-//   return {
-//     title: `title`,
-//     description: `test ${params.id}`,
-//     openGraph: {
-//       images: [detail.img],
-//     },
-//   };
-// }
+export async function generateMetadata({ params }) {
+  const detail = mediaNews.news.find((info) => info.id == params.id);
+
+  return {
+    title: detail.title,
+    description: detail.description,
+    openGraph: {
+      images: [detail.img],
+    },
+  };
+}
 
 async function NewsDetail({ params }) {
   try {
-    const file = await fs.readFile(
-      path.join(process.cwd()) + "/src/data/mediahub.json",
-      "utf8"
-    );
-    const data = JSON.parse(file);
-    const detail = data.news.find((info) => info.id == params.id);
+    const detail = mediaNews.news.find((info) => info.id == params.id);
     return (
       <>
         <div className="news_detail col">
